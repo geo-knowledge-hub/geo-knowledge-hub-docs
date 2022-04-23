9420bc7c4adbf4df70ae2814361c6c96c4dd76de
Felipe Menino <efelipecarlos@gmail.com>
2022-04-23 03:12:12 -0300

    pages: adding support for netlify-cms widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import React from 'react';
+import clsx from 'clsx';
+
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import Head from '@docusaurus/Head';
+
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -28,11 +31,34 @@ function HomepageHeader() {
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+
+  // Add Netlify Identity script
+  if (typeof window !== 'undefined') {
+    // browser code
+    const document = window.document;
+    let script = document.createElement('script');
+    script.innerHTML = `
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          });
+        }
+      `;
+    document.head.appendChild(script);
+  }
+
   return (
     <Layout
-      title={"Documetation"}
+      title={'Documetation'}
       description="GEO Knowledge Hub Documentation page"
     >
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
